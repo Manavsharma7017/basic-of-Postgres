1ad771b384f77414a3db4752bfd3cbe96be56804
Manavsharma7017 <manavsharma7017@gmail.com>
2024-09-25 23:39:56 +0530

    postgres

--- src/show.ts
+++ src/show.ts
@@ -13,5 +13,22 @@ async function show(){
         console.log(result3)
 }catch(e){
     console.log(e)
+}
+finally{
+    client3.end()
 }}
+
+// try {
+//     await client.connect(); // Ensure client connection is established
+//     // Use parameterized query to prevent SQL injection
+//     const insertQuery = "INSERT INTO users (username, email, password) VALUES ($1, $2, $3)";
+//     const values = [username, email, password];
+//     const res = await client.query(insertQuery, values);
+//     console.log('Insertion success:', res); // Output insertion result
+//   } catch (err) {
+//     console.error('Error during the insertion:', err);
+//   } finally {
+//     await client.end(); // Close the client connection
+//   }
+// }
 show()
